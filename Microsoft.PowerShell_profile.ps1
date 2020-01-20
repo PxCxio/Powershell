56dec406cc8f5957fbc77e6d6c801d6f8b62f20f
unknown <33964253+PxCxio@users.noreply.github.com>
2020-01-20 00:03:39 -0600

    Updated Profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,38 @@
-	
-alias vi vim
+set-alias vi vim
 
-function sign ($filename) {
-  $cert = @(gci cert:\currentuser\my 
--codesigning)[0]
-Set-AuthenticodeSignature $filename $cert
+function bar {
+cd C:\Users\xyriux\Documents\pianobar\pianobar-windows-binaries 
+./pianobar
 }
 
+function sign ($filename) {
 
-function ..3 {cd ..\..\..}
-alias .3 ..3
+  $cert = @(gci cert:\currentuser\my 
 
-function mkcd { mkdir -p "$1" && cd "$1";}
-alias mcd mkcd
+-codesigning)[0]
 
-function script:bootToOsX 
-{ 
-    & "C:\Program Files\Boot Camp\Bootcamp.exe" -StartupDisk "Macintosh HD" 
-    restart-computer -force 
-} 
+Set-AuthenticodeSignature $filename $cert
+
+}
+
+function rwebcm ($filename) {
+ffmpeg -f dshow -i video="Integrated Camera" -f dshow -i audio="Microphone Array (Synaptics Audio)" C:\Users\xyriux\Videos\MKV\$filename.mkv
+}
+
+function rdsk ($filename) {
+ffmpeg -f gdigrab -framerate 30 -i desktop C:\Users\xyriux\Videos\MKV\$filename.mkv
+}
+
+function psbmkv2mp4{
+Get-ChildItem -Filter '*.mkv' | % { &ffmpeg -i .\$($_.BaseName).mkv -c copy .\$($_.BaseName).mp4 }
+}
+
+function .3 {cd ..\..\..}
+
+set-alias ..3 .3
+
+
+
+function mkcd ($1) { mkdir -p "$1" ; cd "$1";}
+
+set-alias mcd mkcd
